9c602a93a1e60c699435eccc19aca494a7fdf712
gute1993-idan <igutman93@gmail.com>
2023-06-15 15:45:26 +0300

    google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKFTeEEkCvKBZ9bqnb2w9n_8pSd_ntjRI",
+    authDomain: "angular-chat-3e836.firebaseapp.com",
+    projectId: "angular-chat-3e836",
+    storageBucket: "angular-chat-3e836.appspot.com",
+    messagingSenderId: "135421882203",
+    appId: "1:135421882203:web:ead3386cfccd53c8dbf47e",
+    measurementId: "G-VJ1C27SJC0"
+  }
+};
